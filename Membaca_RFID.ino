82a64e37424f5ee6608ac6354a2012bc3991741f
iki297 <76817825+iki297@users.noreply.github.com>
2021-09-25 10:06:30 +0700

    Add files via upload

---
+++ Membaca_RFID.ino
@@ -0,0 +1,33 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#define SDA 10
+#define RST 9
+MFRC522 RFID(SDA, RST);
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  RFID.PCD_Init();
+  Serial.println("PERCOBAAN MEMBACA KARTU");
+  Serial.println("");
+  Serial.println("SILAHKAN TAP KARTU ANDA");
+  Serial.println();
+}
+void loop() {
+  if ( ! RFID.PICC_IsNewCardPresent()) {
+    return;
+  }
+  if ( ! RFID.PICC_ReadCardSerial()) {
+    return;
+  }
+  Serial.print("ID Tag :");
+  String content = "";
+  byte letter;
+  for (byte i = 0; i < RFID.uid.size; i++) {
+    Serial.print(RFID.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(RFID.uid.uidByte[i], HEX);
+    content.concat(String(RFID.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(RFID.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  delay(1000);
+}
